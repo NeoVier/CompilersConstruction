410fa26b45d0c9b23becc6f89e708ede7b3c9512
Henrique Buss <henrique.buss@hotmail.com>
2021-06-28 19:13:40 -0300

    Get filename from args

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,13 @@
-port module Main exposing (..)
+port module Main exposing (main)
 
 import Json.Decode as Decode
 import Json.Encode as Encode
 import Parser
 import Parser.Program as Program
+
+
+
+-- PORTS
 
 
 port requestFile : String -> Cmd msg
@@ -12,7 +16,14 @@ port requestFile : String -> Cmd msg
 port getFile : (Encode.Value -> msg) -> Sub msg
 
 
-main : Program () Model Msg
+port printError : String -> Cmd msg
+
+
+
+-- MAIN
+
+
+main : Program Encode.Value Model Msg
 main =
     Platform.worker
         { init = init
@@ -21,17 +32,47 @@ main =
         }
 
 
+
+-- MODEL
+
+
 type alias Model =
     { fileContents : Maybe String }
 
 
+init : Encode.Value -> ( Model, Cmd Msg )
+init flags =
+    let
+        filename =
+            case Decode.decodeValue (Decode.list Decode.string) flags of
+                Ok [ x ] ->
+                    Ok x
+
+                Ok [] ->
+                    Err "I received no file names! Please give me exactly one file name!"
+
+                Ok _ ->
+                    Err "I received too many arguments! I only accept a single file name!"
+
+                Err _ ->
+                    Err "I couldn't decode the CLI arguments, make sure you're sending them in correctly!"
+    in
+    ( { fileContents = Nothing }
+    , case filename of
+        Ok file ->
+            requestFile file
+
+        Err err ->
+            printError err
+    )
+
+
+
+-- MSG
+
+
 type Msg
     = GotFile (Result Decode.Error String)
-
-
-init : () -> ( Model, Cmd Msg )
-init _ =
-    ( { fileContents = Nothing }, requestFile "examples/floatAttribution.lcc" )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -51,9 +92,17 @@ update (GotFile fileResult) _ =
     )
 
 
+
+-- SUBSCRIPTIONS
+
+
 subscriptions : Model -> Sub Msg
 subscriptions _ =
     getFile (Decode.decodeValue getFileDecoder >> GotFile)
+
+
+
+-- HELPERS
 
 
 getFileDecoder : Decode.Decoder String
--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 const Elm = require('./elm.js').Elm;
 const fs = require('fs');
 
-const main = Elm.Main.init();
+const cliArgs = process.argv.slice(2);
+
+const main = Elm.Main.init({ flags: cliArgs });
+
+main.ports.printError.subscribe((error) => {
+    console.error(error)
+});
 
 main.ports.requestFile.subscribe((filename) => {
     fs.readFile(filename, 'utf8', (err, fileContents) => {
         if (err) {
-            main.ports.getFile.send({ error: err })
-            return
+            main.ports.getFile.send({ error: err });
+            return;
         }
 
-        main.ports.getFile.send({ fileContents })
+        main.ports.getFile.send({ fileContents });
     });
 });
